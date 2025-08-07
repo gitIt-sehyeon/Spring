c51c058c7af88bda46b3ce8b9bc723b02e25a559
sehyeon <tpgusdla5789@gmail.com>
2025-08-08 00:04:41 +0900

    fix test code

--- core/src/test/java/hello/core/beanfind/AplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/beanfind/AplicationContextExtendsFindTest.java
@@ -17,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AplicationContextExtendsFindTest {
+    
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
 
     @Test
